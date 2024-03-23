03ad17a07d7d973ca9700dfd2a33d997dba47f68
gutter2003 <guybigbig_007@hotmail.com>
2024-03-23 21:20:34 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LADA',
+      title: 'LADA1',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
