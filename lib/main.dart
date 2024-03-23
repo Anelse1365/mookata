5263dac775ad160e51bc36472356b009d89eaf90
Patepan Tudtiang <patepantudting@gmail.com>
2024-03-23 21:51:34 +0700

    Boom LADA2
    yoyo

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'LADA1',
+      title: 'LADA2',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
