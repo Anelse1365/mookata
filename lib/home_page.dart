5aa9e79532906465fd697024331ad778163e1cdb
fangfang0410 <143011529+fangfang0410@users.noreply.github.com>
2024-03-25 19:00:39 +0700

    login

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mookata/reserve/reservation_page.dart';
 import 'package:mookata/payment/payment.dart'; // Import ไฟล์ payment.dart เข้ามา
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatelessWidget {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -58,6 +59,50 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class Firestore_Datasource {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<bool> CreateUser(String email) async {
+    try {
+      await _firestore
+          .collection('users')
+          .doc(_auth.currentUser!.uid)
+          .set({"id": _auth.currentUser!.uid, "email": email});
+      return true;
+    } catch (e) {
+      print(e);
+      return true;
+    }
+  }
+}
+
+abstract class AuthenticationDatasource {
+  Future<void> register(String email, String password, String PasswordConfirm);
+  Future<void> login(String email, String password);
+}
+
+class AuthenticationRemote extends AuthenticationDatasource {
+  @override
+  Future<void> login(String email, String password) async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email.trim(), password: password.trim());
+  }
+
+  @override
+  Future<void> register(
+      String email, String password, String PasswordConfirm) async {
+    if (PasswordConfirm == password) {
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: email.trim(), password: password.trim())
+          .then((value) {
+        Firestore_Datasource().CreateUser(email);
+      });
+    }
   }
 }
 
