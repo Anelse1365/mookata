7ddb9b52737fb9bb0504a90101240e5a34eb0047
fangfang0410 <143011529+fangfang0410@users.noreply.github.com>
2024-03-25 19:17:48 +0700

    still same

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -62,49 +62,7 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class Firestore_Datasource {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<bool> CreateUser(String email) async {
-    try {
-      await _firestore
-          .collection('users')
-          .doc(_auth.currentUser!.uid)
-          .set({"id": _auth.currentUser!.uid, "email": email});
-      return true;
-    } catch (e) {
-      print(e);
-      return true;
-    }
-  }
-}
-
-abstract class AuthenticationDatasource {
-  Future<void> register(String email, String password, String PasswordConfirm);
-  Future<void> login(String email, String password);
-}
-
-class AuthenticationRemote extends AuthenticationDatasource {
-  @override
-  Future<void> login(String email, String password) async {
-    await FirebaseAuth.instance.signInWithEmailAndPassword(
-        email: email.trim(), password: password.trim());
-  }
-
-  @override
-  Future<void> register(
-      String email, String password, String PasswordConfirm) async {
-    if (PasswordConfirm == password) {
-      await FirebaseAuth.instance
-          .createUserWithEmailAndPassword(
-              email: email.trim(), password: password.trim())
-          .then((value) {
-        Firestore_Datasource().CreateUser(email);
-      });
-    }
-  }
-}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
